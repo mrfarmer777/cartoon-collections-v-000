0bfe1c603eaf29e7fbbb7f16e15ff0a0432dffe2
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:46:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,14 @@ def long_planeteer_calls(calls)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(coll)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  coll.each {|item|
+    cheese_types.each{|type|
+      if item==type
+        return type
+      end
+    }
+  }
 end
